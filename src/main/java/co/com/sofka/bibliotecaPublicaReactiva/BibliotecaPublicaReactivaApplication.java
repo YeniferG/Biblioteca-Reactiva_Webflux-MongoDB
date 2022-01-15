10c94512fee9a0d6791103051d68c27032b02ed3
Yenifer <guajeyenifer@gmail.com>
2022-01-14 22:34:19 -0500

    Se inicializa el proyecto de la biblioteca publica con Webflux Routers y MongoDB Reactivo

---
+++ src/main/java/co/com/sofka/bibliotecaPublicaReactiva/BibliotecaPublicaReactivaApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.bibliotecaPublicaReactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaPublicaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaPublicaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/bibliotecaPublicaReactiva/BibliotecaPublicaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.bibliotecaPublicaReactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaPublicaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
